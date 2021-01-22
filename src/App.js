a51b503db597c505850ea171b697069a320be5d3
Achraf <achraf.rehioui.1@gmail.com>
2021-01-22 23:22:49 +0100

    add card component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Cards from './components/Cards/Cards';
 import Chart from './components/Chart/Chart';
 import CountryPicker from './components/CountryPicker/CountryPicker';
 import styles from './App.module.css';
-import { fetchData } from './api/index';
+import { fetchData } from './api/';
 
 class App extends React.Component {
 
