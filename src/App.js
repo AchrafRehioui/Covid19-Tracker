691dba165d2734b8b41c27885eddfa54721fbc14
Achraf <achraf.rehioui.1@gmail.com>
2021-01-22 17:29:41 +0100

    adding api mathdro id

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart';
+import CountryPicker from './components/CountryPicker/CountryPicker';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>App</h1>
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
       </div>
     )
   }
